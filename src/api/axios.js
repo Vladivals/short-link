7ea12c1b560f12339c9e49250347876ea9228724
Vladivals <45815845+Vladivals@users.noreply.github.com>
2022-09-08 09:33:19 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,15 +2,7 @@ import axios from 'axios';
 import App  from '../App';
 import useAuth from '../Hooks/useAuth';
 
-
-
-
-// axios.defaults.baseURL = 'http://79.143.31.216'
-// axios.defaults.headers.common = {'Authorization': `bearer ""`}
-// export default axios;
-
 const BASE_URL = 'http://79.143.31.216';
-
 
 export default axios.create({
     baseURL: BASE_URL
